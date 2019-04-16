fixed an issue with calculating hash when average value of dct was negative fixed displaying large groups, they're now split into few windows added some more images

phash.py
@@ -10,12 +10,6 @@ def p_hash(path, size=(32, 32), show=False):
     if img is None:
         raise RuntimeError
 
-    # FIXME sprawdzić dlaczego przy tym obrazku średnia wartość po transfromacie wychodzi ujemna
-    #   co powoduje że hasz przybiera formę samych jedynek
-    #   miejsce do ustawienia break pointa
-    if path == 'data/krzeslo9_obramowane.png':
-        print("")
-
     img = cv2.resize(img, size)         # resize image to smaller size
     imf = np.float32(img) / 255.        # covert values from 0..255 to 0..1
     dst = cv2.dct(imf)                  # run discrete cosine transform
@@ -32,8 +26,10 @@ def p_hash(path, size=(32, 32), show=False):
     average /= dst.size
 
     # further reduce the dct, set the hash bits to 0 or 1 depending whether they're above or below the average
-    dst[dst < average] = 0
-    dst[dst >= average] = 1
+    mask0 = dst < average
+    mask1 = dst >= average
+    dst[mask0] = 0
+    dst[mask1] = 1
 
     # convert hash bits to integer
     h = 0

presentation.py
@@ -1,9 +1,20 @@
 import numpy as np
 import cv2.cv2 as cv2
+from win32api import GetSystemMetrics
+
+
+def display_img(caption, img, wait_for_key=True):
+    cv2.namedWindow(caption, cv2.WINDOW_NORMAL)
+    cv2.imshow(caption, img)
+    h1, w1 = img.shape[:2]
+    cv2.resizeWindow(caption, w1, h1)
+    if wait_for_key:
+        cv2.waitKey(0)
 
 
 def show_grouped_images(groups, disp_single_groups=True):
     path = "data/"
+    screen_widht = GetSystemMetrics(0)
     for gHash, gImages in groups.items():
         # print(f"{gHash}: {gImages}")
         name = gHash + ': '
@@ -15,27 +26,36 @@ def show_grouped_images(groups, disp_single_groups=True):
                 img1 = cv2.imread(path + image)
                 if img1 is None:
                     raise RuntimeError
-            # if it's second or further image concat it with previous images
+            # if it's second or further image try to concat it with previous images
             else:
                 # https://stackoverflow.com/a/24522170
                 img2 = cv2.imread(path + image)
                 h1, w1 = img1.shape[:2]
                 h2, w2 = img2.shape[:2]
-                # create empty matrix
-                vis = np.zeros((max(h1, h2), w1 + w2, 3), np.uint8)
+                # if the width of combined images would be greater than the width of the screen
+                #   then break them apart
+                if w1 + w2 > screen_widht:
+                    name = name[:-2]
+                    display_img(name, img1, False)
+                    name = gHash + ': '
+                    img1 = img2
+                else:
+                    # create empty matrix
+                    vis = np.zeros((max(h1, h2), w1 + w2, 3), np.uint8)
 
-                # combine 2 images
-                vis[:h1, :w1, :3] = img1
-                vis[:h2, w1:w1 + w2, :3] = img2
-                img1 = vis
+                    # combine 2 images
+                    vis[:h1, :w1, :3] = img1
+                    vis[:h2, w1:w1 + w2, :3] = img2
+                    img1 = vis
 
         name = name[:-2]
-        cv2.namedWindow(name, cv2.WINDOW_NORMAL)
-        cv2.imshow(name, img1)
-        h1, w1 = img1.shape[:2]
-        cv2.resizeWindow(name, w1, h1)
-        if disp_single_groups:
-            cv2.waitKey(0)
+        display_img(name, img1, disp_single_groups)
+        # cv2.namedWindow(name, cv2.WINDOW_NORMAL)
+        # cv2.imshow(name, img1)
+        # h1, w1 = img1.shape[:2]
+        # cv2.resizeWindow(name, w1, h1)
+        # if disp_single_groups:
+        #     cv2.waitKey(0)
 
     if not disp_single_groups:
         cv2.waitKey(0)